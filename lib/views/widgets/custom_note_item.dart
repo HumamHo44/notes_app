e546715cac6596e4bce5fb28011ac54d33546412
HumamHo44 <humaminof@gmail>
2024-05-19 14:16:40 +0300

    Create Edit Note View

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,62 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key, required this.color});
   final Color color;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(
-                top: 16,
-                bottom: 16,
-              ),
-              child: Text(
-                'Build your career with humam alhuri',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
                 style: TextStyle(
-                  color: Colors.black.withOpacity(.5),
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            trailing: Transform.translate(
-              offset: const Offset(0, -25),
-              child: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
                   color: Colors.black,
-                  size: 24,
+                  fontSize: 26,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                  bottom: 16,
+                ),
+                child: Text(
+                  'Build your career with humam alhuri',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(.5),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: Transform.translate(
+                offset: const Offset(0, -25),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 24,
+                  ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May22 , 2024',
-              style:
-                  TextStyle(color: Colors.black.withOpacity(.4), fontSize: 16),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'May22 , 2024',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(.4), fontSize: 16),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
