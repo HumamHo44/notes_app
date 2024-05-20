0a8439211f4599c3f38c9dae35b59b7a38c1e7c7
HumamHo44 <humaminof@gmail>
2024-05-20 23:11:16 +0300

    refiresh notes list

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 
 import 'add_note_form.dart';
 
@@ -19,6 +20,7 @@ class AddNoteBottomSheet extends StatelessWidget {
             print('failied ${state.errMassage}');
           }
           if (state is AddNoteSuccess) {
+            BlocProvider.of<NotesCubit>(context).fetchAllNotes();
             Navigator.pop(context);
           }
         },
